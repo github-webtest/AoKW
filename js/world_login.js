237ed50cf1165d704f4fb73ff7f1e07c86c908a4
github-webtest <lord.47.dark@gmail.com>
2021-04-29 23:03:29 +0300

    index

--- js/world_login.js
+++ js/world_login.js
@@ -5,6 +5,11 @@
 
 var setselect_world_text = localStorage.getItem("setselect_world_text");
 
+function world_0() {
+	localStorage.setItem("setselect_world_text", "0");
+	window.location.href = "index.html";
+};
+
 function world_1() {
 	localStorage.setItem("setselect_world_text", "1");
 	window.location.href = "index.html";
